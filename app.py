39ad83aa396806daa697837494736e28c51b08b2
Lucabrando Sanfilippo <sanfilippo.lucabrando@gmail.com>
2024-07-24 09:35:40 +0200

    Update codebase

--- app.py
+++ app.py
@@ -45,6 +45,4 @@ def index():
     
     return render_template('index.html', summary=summary)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+# No need for the if __name__ == "__main__" block for Vercel deployment
